0e0a10b757ebbe560632284c5bfc6855ebb0d47f
Jorge Ariel Castillo <j.a.castillo.jc@gmail.com>
2023-06-10 15:39:12 -0300

    remove burger menu

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './navbar.scss';
 
 export const Sidebar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-      const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
 
     return (
         <nav className='navbar'>
@@ -23,17 +16,6 @@ export const Sidebar = () => {
                         <a className='navbar_list_item' href="#contact">Contacto</a>
                     </ul>
             
-                {/* <div id="burger-menu" className={isMenuOpen ? 'close' : ''} onClick={toggleMenu}>
-                    <span></span>
-                </div>
-                <div id="menu" className={isMenuOpen ? 'overlay' : ''}>
-                    <ul>
-                        <a href="#home" onClick={closeMenu}>Home</a>
-                        <a href="#about" onClick={closeMenu}>Acerca de mí</a>
-                        <a href="#projects" onClick={closeMenu}>Proyectos</a>
-                        <a href="#contact" onClick={closeMenu}>Contacto</a>
-                    </ul>
-                </div> */}
         </nav> 
 
   );
